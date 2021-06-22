32680a85dbe79ec592a66fc3a2976c4921ee30bd
buchuitoudegou <756541536@qq.com>
2021-06-22 15:47:19 +0800

    question cond

--- src/context/condition.h
+++ src/context/condition.h
@@ -1,7 +1,7 @@
 #ifndef __CONDITION_H__
 #define __CONDITION_H__
 
-// 16-bit condition flag
+// 16-bit flags controlling the features of the code
 #define LOGIC_COMP 0x1
 #define BRANCH_SEL 0x2
 #define MACRO 0x4
@@ -10,5 +10,19 @@
 #define FOR_LOOP 0x20
 #define FUNC_CALL 0x40
 #define FUNC_DEF 0x80
+
+// question generation: question form
+#define MCQ 0x1
+#define FILL_BLK 0x2
+#define REORDER 0x4
+
+// question generation: question type
+#define PRED_OUTPUT 0x1
+#define IDT_SE 0x2
+#define IDT_RE 0x4
+
+// question generation: error
+#define IDX_OUT_OF_BOUND 0x1
+#define MIS_SEMI 0x2
 
 #endif
