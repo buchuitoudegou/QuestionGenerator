20709862860a4a157f072e35f6659df1072cc351
buchuitoudegou <756541536@qq.com>
2021-07-06 16:58:09 +0800

    feat: question generation

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,13 @@
 #include "context/condition.h"
 #include <fstream>
 #include <toml.hpp>
+#include <unistd.h>
+#include <fcntl.h>
+#include <iterator>
 
+using std::istream_iterator;
 using std::ofstream;
+using std::ifstream;
 using toml::parse;
 
 uint16_t gen_flags(const decltype(parse(""))& data) {
@@ -50,12 +55,32 @@ uint16_t gen_flags(const decltype(parse(""))& data) {
   return flags;
 }
 
+void dup_option(vector<vector<int>>& options) {
+  if (options.size() == 0) {
+    for (int i = 0; i < 3; ++i) {
+      int a1 = rand() % 100, a2 = rand() % 100;
+      vector<int> tmp{a1, a2};
+      options.push_back(tmp);
+    }
+  } else {
+    for (int i = 0; i < 2; ++i) {
+      int a1 = i == 0? options[0][0]: rand() % 100;
+      int a2 = i == 1? options[0][1]: rand() % 100;
+      vector<int> tmp{a1, a2};
+      options.push_back(tmp);
+    }
+  }
+}
+
 int main(int argc, const char* argv[]) {
   srand(time(NULL));
   const char* cfg_path = "../default.toml";
   if (argc > 1) {
     cfg_path = argv[1];
   }
+  /*
+   *  generate code w.r.t the input  
+   */
   const auto data  = parse(cfg_path);
   auto st_cnt = toml::find<int>(data, "statement_cnt");
   auto v_cnt = toml::find<int>(data, "variable_cnt");
@@ -63,10 +88,56 @@ int main(int argc, const char* argv[]) {
   uint16_t flags = gen_flags(data);
   Context* ctx = generate_context(flags, v_cnt, st_cnt, v_app_r);
   string code = code_generation(ctx);
-  ofstream o_file;
-  o_file.open("output.cpp");
+  ofstream o_file("output.cpp");
   o_file << code;
   o_file.close();
   delete ctx;
+  /*
+   *  compile the generated code and get the output
+   */
+  int fd = open("out.txt", O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU); // create output file
+  auto pid = fork(); // compile the code in a child process
+  if (pid == 0) {
+    execlp("g++", "g++", "output.cpp", "-o", "output", "-w", NULL);
+  }
+  while (waitpid(pid, NULL, WNOHANG) == 0); // wait for the child ending
+  int save_stdout = dup(STDOUT_FILENO); // save stdout
+  close(STDOUT_FILENO); // redirect the standard output to a file (printf->file)
+  dup(fd); // redirecting
+  pid = fork();
+  if (pid == 0) {
+    execlp("./output", "./output", NULL); // executing the binary file
+  }
+  while (waitpid(pid, NULL, WNOHANG) == 0); // wait for the child ending
+  close(fd); // finish processing
+  close(STDOUT_FILENO); // finish processing
+  dup2(save_stdout, STDOUT_FILENO); // re-open the stdout
+  close(save_stdout);
+  /*
+   * generate the question and the distractors with the output
+   */
+  ifstream ifs("out.txt");
+  istream_iterator<int> it_ifs(ifs), eof;
+  vector<int> answer(it_ifs, eof); // correct answer
+  string question = "What is the output of the following code: \n";
+  question += code + "\n";
+  vector<vector<int>> options;
+  if (answer.size() > 0) {
+    options.push_back(answer);
+  }
+  dup_option(options);
+  for (int i = 0; i < options.size(); ++i) {
+    question.push_back('A' + i);
+    question += ". ";
+    for (auto& ans: options[i]) {
+      question += std::to_string(ans) + " ";
+    }
+    question += "\n";
+  }
+  question += "D. RUNTIME ERROR\n";
+  ofstream q_str("question.txt", ofstream::trunc);
+  q_str << question << std::endl;
+  q_str.close();
+  ifs.close();
   return 0;
 }
